a5928849158e6e3a8ad55897179ef0399fbab04a
tcollins527 <tylercollins727@gmail.com>
2024-05-13 18:45:53 -0500

    this'll probably break it

--- fetch_data.py
+++ fetch_data.py
@@ -27,7 +27,7 @@ def fetch_census_data():
     first_of_this_month = today.replace(day=1)
     last_month = first_of_this_month - timedelta(days=1)
     year = last_month.year
-    month = last_month.strftime("%b").lower()
+    month = last_month.strftime("%b").capitalize()
 
     url = f"{base_url}/{year}/cps/basic/{month}?get={','.join(vars)}&for=state:*&key=0cbe74e555fe4b3dac32591deacb7248f4dafae0"
 
@@ -37,21 +37,45 @@ def fetch_census_data():
     data = response.json()
     df = pd.DataFrame(data[1:], columns = data[0])
 
+    df['year'] = year
+    df['month'] = month
+
+    column_order = ['HRHHID', 'HWHHWGT', 'year', 'month', 'REGION', 'STATE', 'CBSA', 'PELAYFTO', 'PELAYDUR', 'PESEX', 'PTDTRACE', 'PEEDUCA', 'PEMARITL', 'HEFAMINC', 'HRHTYPE', 'HRNUMHOU']
+    df = df[column_order]
+
+    return df
+
+def filter_data(df):
+    df = df[df['PELAYFTO'].isin(['0','1'])]
     return df
 
 def save_data(df, year, month):
-    filename = f"data/cps_data_{year}_{month}.csv"
+    filename = f"data/layoff_data_{year}_{month}.csv"
     df.to_csv(filename, index=False)
     print(f"Data saved to {filename}")
 
+# def filter_data(df):
+#     df = df[df['PELAYFTO'].isin(['Yes'])]
+
 def main():
-    df = fetch_census_data()
+    new_df = fetch_census_data()
+    new_df = filter_data(new_df)
+
+    # drop header to avoid remapping
+    new_df = new_df.iloc[1:]
+
+    # load existing data
+    existing_df = pd.read_csv('data/layoff_data.csv')
+
+    combined_df = pd.concat([existing_df, new_df]).drop_duplicates()
+
+
     today = datetime.now()
     first_of_this_month = today.replace(day=1)
     last_month = first_of_this_month - timedelta(days=1)
     year = last_month.year
-    month = last_month.strftime("%m")
-    save_data(df, year, month)
+    month = last_month.strftime("%b").capitalize()
+    save_data(combined_df, year, month)
 
 if __name__ == "__main__":
     main()
